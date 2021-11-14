65a92dfe51d3b86d11d63530a0797690114c6f95
Anurag nayak <68118269+Anurag-345@users.noreply.github.com>
2021-11-14 20:57:01 +0530

    initial commit

---
+++ test 14 nov/index.js
@@ -0,0 +1,37 @@
+var SS1 = document.getElementById('i1');
+        var SS2 = document.getElementById('i2');
+        var SS3 = document.getElementById('i3');
+        function fun1() {
+            i1.style.color = "green";
+            i1.innerText = "YES";
+
+        }
+
+        function fun2() {
+            i2.style.color = "green";
+
+            i2.innerText = "YES";
+
+
+        }
+        function fun3() {
+            i3.style.color = "green";
+
+            i3.innerText = "YES";
+
+        }
+        function d1() {
+            i1.style.color = "red";
+            i1.innerText = "NO";
+
+        }
+        function d2() {
+            i2.style.color = "red";
+            i2.innerText = "NO";
+
+        }
+        function d3() {
+            i3.style.color = "red";
+            i3.innerText = "NO";
+
+        }
