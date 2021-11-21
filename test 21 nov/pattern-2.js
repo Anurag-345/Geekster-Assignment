8704db299e2a36d9e2026154faa10aa7fc1d72b6
Anurag nayak <68118269+Anurag-345@users.noreply.github.com>
2021-11-21 22:00:04 +0530

    initial commit

---
+++ test 21 nov/pattern-2.js
@@ -0,0 +1,14 @@
+let n= 5;
+function star(a) {
+    var String = "";
+    for(i =1; i <= 5; i++){
+        for(j =1; j <= i; j++){
+        String += "*";
+    }
+        String += "\n";
+    
+    }
+   console.log(String);
+}
+star(n);
+
